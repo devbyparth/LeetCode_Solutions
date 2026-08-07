5711f315a4d923a47e26cbc9021b839adc920a52
Parth Verma <parthverma.codes@gmail.com>
2026-08-07 12:27:05 +0530

    Time: 954 ms (20.38%), Space: 18.3 MB (58.15%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+
+        result_list = []
+
+        for i in range(len(nums)):
+
+            if i != 0 and nums[i] == nums[i - 1]:
+                continue
+
+            j, k = i + 1, len(nums) - 1
+
+            while j < k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j = j+1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k = k-1
+                else:
+                    result_list.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k+1]:
+                        k -= 1
+        
+        return result_list
