0885952d481118737395ffd755802b22f90389bf
Parth Verma <parthverma.codes@gmail.com>
2026-08-07 13:00:03 +0530

    Time: 0 ms (100%), Space: 13.2 MB (71.62%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def search(self, nums, target):
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            elif target < nums[mid]:
+                high = mid - 1
+
+            elif target > nums[mid]:
+                low = mid + 1
+
+        return -1
