7931111bcf9a9e7a07afd3f2b8044dd65b7f7ce5
Parth Verma <parthverma.codes@gmail.com>
2026-08-11 20:45:54 +0530

    Time: 125 ms (18.42%), Space: 21.6 MB (71.05%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from math import ceil
+
 
 class Solution(object):
     def minimumRounds(self, tasks):
@@ -9,6 +11,8 @@ class Solution(object):
         for v in freqMap.values():
             if v == 1:
                 return -1
-            rounds += -(-v // 3)
-
+            if v % 3 == 0:
+                rounds += v // 3
+            else:
+                rounds += (v // 3) + 1
         return rounds
