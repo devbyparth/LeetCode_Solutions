35ed6b0355435192fa76fca541289d6a9437f1c2
Parth Verma <parthverma.codes@gmail.com>
2026-08-13 11:06:16 +0530

    Time: 143 ms (97.9%), Space: 13.6 MB (66.08%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -3,12 +3,17 @@ class Solution(object):
         if len(arr) < 3:
             return False
 
-        left, right = 0, len(arr) - 1
+        i = 0
+        while i+1 < len(arr) and arr[i] < arr[i + 1]:
+            i += 1
 
-        while left+1 < len(arr) and arr[left] < arr[left+1]:
-            left += 1
+        if i == 0 or i == len(arr) - 1:
+            return False
 
-        while right-1 > 0 and arr[right-1] > arr[right]:
-            right -= 1
+        while i+1 < len(arr) and arr[i] > arr[i+1]:
+            i += 1
 
-        return left == right and left != 0 and right != len(arr)-1
+        if i == len(arr) - 1:
+            return True
+        else:
+            return False
