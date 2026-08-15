facba4cc7d35334a7c772d157051a96c084d0b4c
Parth Verma <parthverma.codes@gmail.com>
2026-08-15 18:20:36 +0530

    Time: 0 ms (100%), Space: 12.4 MB (62.08%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def gameOfLife(self, board):
+        # Original | New | State
+        #    0     |  0  |  0
+        #    1     |  0  |  1
+        #    0     |  1  |  2
+        #    1     |  1  |  3
+
+        ROWS, COLS = len(board), len(board[0])
+
+        def count_neighbors(r, c):
+            neighbors = 0
+            for i in range(r - 1, r + 2):
+                for j in range(c - 1, c + 2):
+                    if (i == r and j == c) or i < 0 or j < 0 or i == ROWS or j == COLS:
+                        continue
+                    if board[i][j] in {1, 3}:
+                        neighbors += 1
+            return neighbors
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                neighbors = count_neighbors(r, c)
+
+                if board[r][c] == 1:
+                    if neighbors in {2, 3}:
+                        board[r][c] = 3
+                elif neighbors == 3:
+                    board[r][c] = 2
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 1:
+                    board[r][c] = 0
+                elif board[r][c] in {2, 3}:
+                    board[r][c] = 1
