6128bda6eb3c044b6ba70026b42642b826a30010
Parth Verma <parthverma.codes@gmail.com>
2026-08-20 17:21:38 +0530

    Time: 51 ms (46.65%), Space: 14.5 MB (25.76%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution(object):
+    def findLHS(self, nums):
+        freq = Counter(nums)
+        max_length = 0
+
+        for key in freq:
+            if key+1 in freq:
+                max_length = max(max_length, freq[key] + freq[key + 1])
+
+        return max_length
