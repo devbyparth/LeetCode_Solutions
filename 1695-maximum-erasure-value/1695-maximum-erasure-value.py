4448df87aa351e4a5357c22c5bcf0833ef87a877
Parth Verma <parthverma.codes@gmail.com>
2026-08-22 09:06:08 +0530

    Time: 258 ms (76.1%), Space: 21.2 MB (27.32%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -1,25 +1,17 @@
-from collections import defaultdict
 class Solution(object):
     def maximumUniqueSubarray(self, nums):
-        max_sum = float('-inf')
-        window_arr = defaultdict(int)
+        seen = set()
 
-        left, right = 0, 0
-        cur_sum = 0
+        left, cur_sum, max_sum = 0, 0, 0
 
-        while right < len(nums):
-            incoming = nums[right]
-            cur_sum += incoming
-
-            window_arr[incoming] += 1
-
-            while len(window_arr) < (right - left + 1):
-                outgoing = nums[left]
+        for right in range(len(nums)):
+            while nums[right] in seen:
+                seen.remove(nums[left])
+                cur_sum -= nums[left]
                 left += 1
-                cur_sum -= outgoing
-                window_arr[outgoing] -= 1
-                if window_arr[outgoing] == 0:
-                    del window_arr[outgoing]
-            right += 1
+            
+            seen.add(nums[right])
+            cur_sum += nums[right]
             max_sum = max(max_sum, cur_sum)
+        
         return max_sum
