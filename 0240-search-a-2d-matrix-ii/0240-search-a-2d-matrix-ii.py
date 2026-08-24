8a5269eb83b6033244dd04e1dbbb123dcaad8a0a
Parth Verma <parthverma.codes@gmail.com>
2026-08-24 18:33:38 +0530

    Time: 133 ms (44.18%), Space: 18.4 MB (49.6%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        row = 0
+        col = len(matrix[0])-1
+        
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+        return False
