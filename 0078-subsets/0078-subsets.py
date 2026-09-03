4fa3c307d571da352ef381fb1fedd0ebc4640108
Parth Verma <parthverma.codes@gmail.com>
2026-09-03 14:21:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (28.42%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,21 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-
-        def backtrack(start_index, curr_path):
-            res.append(list(curr_path))
-
-            for i in range(start_index, len(nums)):
-                curr_path.append(nums[i])      # Choose
-                backtrack(i + 1, curr_path)    # Explore
-                curr_path.pop()                # Unchoose (Backtrack)
+    def get_subsets(self, nums, path, i):
+        # Base case: reached the end of nums, return a list containing this subset copy
+        if i == len(nums):
+            return [path[:]]
         
-        backtrack(0, [])
-        return res
+        # Branch 1: Include nums[i]
+        path.append(nums[i])
+        with_num = self.get_subsets(nums, path, i + 1)
+        
+        # Backtrack
+        path.pop()
+        
+        # Branch 2: Exclude nums[i]
+        without_num = self.get_subsets(nums, path, i + 1)
+        
+        # Combine subsets from both decisions and return
+        return with_num + without_num
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        return self.get_subsets(nums, [], 0)
