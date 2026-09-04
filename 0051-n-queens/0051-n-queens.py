89f7a81d7e76b0c055c16c77dda42da6ac1419ab
Parth Verma <parthverma.codes@gmail.com>
2026-09-04 10:53:01 +0530

    Time: 10 ms (72.83%), Space: 19.8 MB (35.45%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,41 +1,36 @@
 class Solution:
-    def is_safe(self, board, row, col, n):
-        # Check column above current row
-        for i in range(row):
-            if board[i][col] == 'Q':
-                return False
-        
-        # Check upper-left diagonal
-        i, j = row - 1, col - 1
-        while i >= 0 and j >= 0:
-            if board[i][j] == 'Q':
-                return False
-            i -= 1
-            j -= 1
-            
-        # Check upper-right diagonal
-        i, j = row - 1, col + 1
-        while i >= 0 and j < n:
-            if board[i][j] == 'Q':
-                return False
-            i -= 1
-            j += 1
-            
-        return True
-
-    def nQueens(self, board, row, n, ans):
-        if row == n:
-            ans.append(["".join(r) for r in board])
-            return
-
-        for j in range(n):
-            if self.is_safe(board, row, j, n):
-                board[row][j] = 'Q'
-                self.nQueens(board, row + 1, n, ans)
-                board[row][j] = '.'
-
     def solveNQueens(self, n: int) -> List[List[str]]:
+        cols = set()
+        pos_diag = set()  # (row + col)
+        neg_diag = set()  # (row - col)
+
         ans = []
-        board = [['.' for _ in range(n)] for _ in range(n)]
-        self.nQueens(board, 0, n, ans)
+        board = [["."] * n for _ in range(n)]
+
+        def backtrack(r: int):
+            if r == n:
+                ans.append(["".join(row) for row in board])
+                return
+
+            for c in range(n):
+                # O(1) checks using math properties
+                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+                    continue
+
+                # Add state
+                cols.add(c)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+                board[r][c] = "Q"
+
+                # Recurse for next row
+                backtrack(r + 1)
+
+                # Backtrack state
+                cols.remove(c)
+                pos_diag.remove(r + c)
+                neg_diag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
         return ans
