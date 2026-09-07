d32c8c5765f30e45b74a2fb79cf2f3dc718c3e7f
Parth Verma <parthverma.codes@gmail.com>
2026-09-07 21:09:14 +0530

    Time: 51 ms (48.47%), Space: 19.3 MB (19.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        return self.fib(n-1) + self.fib(n-2)
+        memo = {}
+
+        def helper(k: int) -> int:
+            if k <= 1:
+                return k
+            if k in memo:
+                return memo[k]
+
+            memo[k] = helper(k - 1) + helper(k - 2)
+            return memo[k]
+
+        return helper(n)
